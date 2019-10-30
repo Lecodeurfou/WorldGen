41a59a97c4d44edf8cc46a27f71633195f7d7263
Roman <roman.boursier@gmail.com>
2019-10-30 21:01:20 +0100

    Add node/express server

---
+++ start.js
@@ -0,0 +1,64 @@
+const http = require('http');
+const path = require("path");
+const spawn = require('child_process').spawn;
+const express = require('express')
+const app = express()
+
+var server = http.Server(app);
+
+// Serv Client
+app.get('/client-test', (req, res) => {
+    res.sendFile(__dirname + '/client-test.html');
+})
+
+// Serv 3d generator
+app.get('/3d-generator', (req, res) => {
+    res.sendFile(__dirname + '/3d-generator/display.html');
+})
+app.get('/scripts/app.js',function(req,res){
+    res.sendFile(path.join(__dirname + '/3d-generator/scripts/app.js')); 
+});
+app.get('/models/shop.glb',function(req,res){
+    res.sendFile(path.join(__dirname + '/3d-generator/models/shop.glb')); 
+});
+app.get('/models/building2.glb',function(req,res){
+    res.sendFile(path.join(__dirname + '/3d-generator/models/building2.glb')); 
+});
+
+
+var io = require('socket.io').listen(server);
+
+// Quand un client se connecte, on le note dans la console
+io.sockets.on('connection', function (socket) {
+
+    console.log('1 - Un client est connecté !');
+
+    socket.on('geolocDatas', function (datas) {
+
+        console.log('2 - GeoDatas reçus ');
+    
+        geoDatas = {
+            lat: datas.lat, 
+            lng: datas.lng
+        };
+        
+        console.log('3 - Lancement du ml');
+
+        py = spawn('python', ['machine-learning/main.py']);
+        py.stdout.on('data', function(data){
+            console.log('4 - Envois au js');
+            socket.broadcast.emit('generateWorld', data.toString());
+        });
+    
+    });
+   
+    socket.on('imgBase64', function (img) {
+        console.log('5 - Js terminé reception de l\'image');
+        socket.broadcast.emit('imgBase64', img);
+    });
+
+});
+
+server.listen(8080, () => {
+    console.log('Express lancé')
+})
